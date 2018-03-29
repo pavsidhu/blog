06f91842f8407640284867800d0ee0389b26dade
Pav Sidhu <pav@pavsidhu.com>
2018-03-29 23:39:39 +0100

    Use gatsby-starter-blog-no-styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,40 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import Link from 'gatsby-link'
 
-import Header from '../components/Header'
-import './index.css'
-
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
-
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+class Template extends React.Component {
+  render() {
+    const { location, children } = this.props
+    let header
+    if (location.pathname === '/') {
+      header = (
+        <h1>
+          <Link to={'/'} >
+            Gatsby Starter Blog
+          </Link>
+        </h1>
+      )
+    } else {
+      header = (
+        <h3>
+          <Link to={'/'}>
+            Gatsby Starter Blog
+          </Link>
+        </h3>
+      )
+    }
+    return (
+      <div>
+        {header}
+        {children()}
+      </div>
+    )
+  }
 }
 
-export default TemplateWrapper
+Template.propTypes = {
+  children: React.PropTypes.func,
+  location: React.PropTypes.object,
+  route: React.PropTypes.object,
+}
+
+export default Template
