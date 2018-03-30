e23af813b165aebe7236dcd8970709fc8911b75a
Pav Sidhu <pav@pavsidhu.com>
2018-03-30 17:23:49 +0100

    Replace styles.css with styled-components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import Header from '../components/Header'
+import { injectGlobal } from 'styled-components'
 
-import './styles.css'
+injectGlobal`
+  html {
+    font-size: 10px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`
 
 class Template extends React.Component {
   render() {
