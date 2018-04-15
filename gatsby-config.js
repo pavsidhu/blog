b8f791eb5d0f94dd6816ef67208b3d12bde57498
Pav Sidhu <pav@pavsidhu.com>
2018-04-15 20:42:13 +0100

    Add gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,25 +5,25 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 700,
               linkImagesToOriginal: false,
             },
           },
           {
-            resolve: `gatsby-remark-embed-youtube`,
+            resolve: 'gatsby-remark-embed-youtube',
             options: {
               width: 800,
               height: 400,
@@ -33,19 +33,20 @@ module.exports = {
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
           'gatsby-remark-responsive-iframe',
+          'gatsby-plugin-styled-components',
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        //trackingId: 'ADD YOUR TRACKING ID HERE',
       },
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-typescript`,
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-typescript',
   ],
 }
